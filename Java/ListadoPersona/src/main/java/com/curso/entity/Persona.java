65d9cbc8ba2942d554e91a1b47a42c86a4e04b37
EfrainCuahutle <efrain337@hotmail.com>
2024-11-21 20:30:09 -0600

    agramos archivos y clases faltantes para la practica de listado de personas

--- Java/ListadoPersona/src/main/java/com/curso/entity/Persona.java
+++ Java/ListadoPersona/src/main/java/com/curso/entity/Persona.java
@@ -1,4 +1,52 @@
 package com.curso.entity;
 
 public class Persona {
+
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    public Persona(String nombre, String tel, String email) {
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getNumeroPersonas() {
+        return numeroPersonas;
+    }
+
+    public void setNumeroPersonas(int numeroPersonas) {
+        this.numeroPersonas = numeroPersonas;
+    }
 }
