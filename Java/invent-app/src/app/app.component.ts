10f4a15355e46da52070616e967c6e8cf0d614eb
EfrainCuahutle <efrain337@hotmail.com>
2025-01-08 17:33:22 -0600

    aplicacion de inventario en angular

--- Java/invent-app/src/app/app.component.ts
+++ Java/invent-app/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from "./product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [ProductListComponent]
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'inventario-app';
+  title = 'invent-app';
 }
--- Java/inventario-app/src/app/product-list/product-list.component.ts
+++ Java/inventario-app/src/app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductService } from '../product.service';
+
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
+
+  productos : Producto[];
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(){
+    //Obtenemos los productos
+    this.getProducts();
+  }
+
+  private getProducts(){
+    //Consumir los datos del observable(suscribirnos)
+    this.productService.getProducList().subscribe(
+      (datos => {
+        this.productos = datos;
+      })
+    )
+  }
 
 }
